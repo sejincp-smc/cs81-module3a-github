ddec49de11b3e4b6b838f3775b7d21732cc89817
Sejin Park <park_sejin02@student.smc.edu>
2025-07-06 06:46:24 -0700

    Add functions

--- journal.js
+++ journal.js
@@ -2,3 +2,32 @@
 function greet(name) {
   return "Hello, " + name + "!";
 }
+
+// Function 1
+function intro(className) {
+  return "This is " + className + "class.";
+}
+
+// Function 2
+function plusOne(x) {
+  return x + 1;
+}
+
+// Function 3
+function convertDegree(x) {
+  return x + "°F is "+ (x-32)*5/9 + "°C.";
+}
+
+// Function 4
+function square(x) {
+  return x * x;
+}
+
+// Function 5
+function signal(color) {
+  if (color === "green") {
+    return "Go";
+  } else {
+    return "Stop";
+  };
+}
